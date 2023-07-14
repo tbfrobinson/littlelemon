bbc615cce3e58d6609acdea831568fc881f55dad
Theo <tbfrobinson@gmail.com>
2023-07-13 22:36:56 -0700

    add Views and viewsets for menuitems and booking

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+
+from reservation.views import BookingViewSet
+
+router = routers.DefaultRouter()
+router.register(r'tables', BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reservation/', include('reservation.urls')),
+    path('restaurant/', include('reservation.urls')),
+    path('restaurant/booking/', include(router.urls))
 ]
