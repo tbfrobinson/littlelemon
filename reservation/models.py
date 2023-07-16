f492415ed8f367f00230b98f7e42ceb0b5536ef1
Theo <tbfrobinson@gmail.com>
2023-07-15 18:08:04 -0700

    add stuff

--- reservation/models.py
+++ reservation/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 # Create your models here.
-class Menu(models.Model):
+class MenuItem(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.SmallIntegerField()
 
-    def get_item(self):
-        return f'{self.title} : {str(self.price)}'
+    # def get_item(self):
+    #     return f'{self.title} : {str(self.price)}'
     
     def __str__(self):
         return f'{self.title} : {str(self.price)}'
@@ -18,4 +18,4 @@ class Booking(models.Model):
     booking_date = models.DateField(db_index=True)
 
     def __str__(self):
-        return f'{self.name} with {str(self.no_of_guests)} others on {self.booking_date}'
+        return f'{self.name} with {str(self.no_of_guests)}, on {self.booking_date}'
--- reservation/tests.py
+++ reservation/tests.py
@@ -1,3 +1,27 @@
+from datetime import datetime
+
 from django.test import TestCase
 
+from .models import MenuItem, Booking
+from .serializers import MenuItemSerializer
+
 # Create your tests here.
+# class MenuItemTest(TestCase):
+#     def test_get_item(self):
+#         item = MenuItem.objects.create(title="IceCream", price=80, inventory=100)
+#         self.assertEqual(str(item), "IceCream : 80")
+
+# class BookingTest(TestCase):
+#     def test_get_booking(self):
+#         item = Booking.objects.create(name="Test", no_of_guests=1, booking_date=datetime.today().date())
+        # self.assertEqual(str(item), "Test with 1, on 2023-07-15")
+
+class MenuViewTest(TestCase):
+    def setup(self):
+        MenuItem.objects.create(title="IceCream", price=80, inventory=100)
+        MenuItem.objects.create(title="Beverage", price=10, inventory=100)
+    def test_getall(self):
+        getitems = MenuItem.objects.all()
+        serializer = MenuItemSerializer(getitems)
+        data = serializer.data
+        self.assertEqual(data, {"IceCream : 80", "Beverage : 10"})
